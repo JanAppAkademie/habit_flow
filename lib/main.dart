8665d982f3c01b4f3cc3b4bedff2b6b7cd59a409
Bahri <bahriakcora622@gmail.com>
2025-12-16 21:40:01 +0100

    supabase added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:habit_flow/app.dart';
 import 'package:habit_flow/models/habit.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+const supabaseUrl = 'https://iqwzkcxwiyqinrlnlitq.supabase.co';
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlxd3prY3h3aXlxaW5ybG5saXRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5MDkxMzQsImV4cCI6MjA4MTQ4NTEzNH0.xRGZ79flq4UaAUsbMA9WOgCNmgzyjl9rwA8cW5zg4A8";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,5 +16,9 @@ void main() async {
 
   Hive.registerAdapter(HabitAdapter());
   await Hive.openBox<Habit>('habits');
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseKey,
+  );
   runApp(ProviderScope(child: const App()));
 }
